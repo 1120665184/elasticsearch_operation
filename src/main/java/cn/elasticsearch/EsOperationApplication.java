dfa738b88cc37690fbc3f7252b55203309d9f4f0
QuYongq <1120665184@qq.com>
2019-07-29 17:46:28 +0800

    .

--- src/main/java/cn/elasticsearch/EsOperationApplication.java
+++ src/main/java/cn/elasticsearch/EsOperationApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @Slf4j
 public class EsOperationApplication {
-
+    //https://www.elastic.co/guide/en/elasticsearch/client/java-rest/7.2/java-rest-high-document-get.html
     public static void main(String[] args) {
         SpringApplication.run(EsOperationApplication.class, args);
     }
